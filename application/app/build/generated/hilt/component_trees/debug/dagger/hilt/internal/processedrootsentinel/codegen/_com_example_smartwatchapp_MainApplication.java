105bf8251866f7b5166bccd96577f78d13ad0300
Mokhtar SAMY <60510677+MokhtarSamy@users.noreply.github.com>
2022-12-03 22:10:07 +0100

    Merge DB + application

---
+++ application/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_smartwatchapp_MainApplication.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.example.smartwatchapp.MainApplication"
+)
+public final class _com_example_smartwatchapp_MainApplication {
+}
---
+++ application/app/build/generated/source/kapt/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_example_smartwatchapp_MainApplication.java
@@ -0,0 +1,19 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.example.smartwatchapp.MainApplication",
+    rootPackage = "com.example.smartwatchapp",
+    originatingRoot = "com.example.smartwatchapp.MainApplication",
+    originatingRootPackage = "com.example.smartwatchapp",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "MainApplication",
+    originatingRootSimpleNames = "MainApplication"
+)
+public class _com_example_smartwatchapp_MainApplication {
+}
