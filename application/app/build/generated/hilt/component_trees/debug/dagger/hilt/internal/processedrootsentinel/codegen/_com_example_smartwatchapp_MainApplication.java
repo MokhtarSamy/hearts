ecffb1c0ddbfa0ca644e62eb6fd28b987a5e7d70
EvaRadu <evaradu@hotmail.fr>
2022-12-03 23:06:31 +0100

    rm files

--- application/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_smartwatchapp_MainApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.smartwatchapp.MainApplication"
-)
-public final class _com_example_smartwatchapp_MainApplication {
-}
