6bf42ced8936ef92107d3eac1ed7ae360048ab0a
Mokhtar SAMY <60510677+MokhtarSamy@users.noreply.github.com>
2022-12-06 23:07:39 +0100

    ADD: Angular chartJs + AngumarFire

---
+++ application/dashboard/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AppComponent } from './app.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4phyR2v8R-MyW0wLDUB2TdenPCyJ8P_o",
+  authDomain: "smartwatchapp-92b44.firebaseapp.com",
+  databaseURL: "https://smartwatchapp-92b44-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "smartwatchapp-92b44",
+  storageBucket: "smartwatchapp-92b44.appspot.com",
+  messagingSenderId: "516324080387",
+  appId: "1:516324080387:web:aebaff936220290aac8736",
+  measurementId: "G-K9KNZRCF73"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarChartComponent,
+    LineChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
