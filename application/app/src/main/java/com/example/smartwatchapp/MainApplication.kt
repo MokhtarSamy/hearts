b7c3fd4106a8125566784713ee4dd5d6876074e9
MiaSwery <91131345+MiaSwery@users.noreply.github.com>
2022-12-13 13:32:17 +0100

    mergee

--- application/app/src/main/java/com/example/smartwatchapp/MainApplication.kt
+++ application/app/src/main/java/com/example/smartwatchapp/MainApplication.kt
@@ -45,4 +45,5 @@ class MainApplication : Application(), Configuration.Provider {
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(PREFERENCES_FILENAME)
 
+
 const val TAG = "Passive Data Sample"
--- application/app/src/main/java/com/example/smartwatchapp/PassiveDataService.kt
+++ application/app/src/main/java/com/example/smartwatchapp/PassiveDataService.kt
@@ -110,10 +110,6 @@ class PassiveDataService : PassiveListenerService() {
 
             }
 
-
-
-
-
     }
 
     // upload data in the "stats" collection
