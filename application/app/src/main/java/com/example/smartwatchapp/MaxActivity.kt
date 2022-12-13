d205275d5da8c180de077b4c35e0bf4db8161716
MiaSwery <91131345+MiaSwery@users.noreply.github.com>
2022-12-13 12:48:13 +0100

    add functions to retrieve data

--- application/app/src/main/java/com/example/smartwatchapp/MaxActivity.kt
+++ application/app/src/main/java/com/example/smartwatchapp/MaxActivity.kt
@@ -3,9 +3,27 @@ package com.example.smartwatchapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 
 class MaxActivity : AppCompatActivity() {
+
+    private fun getData(){
+
+        FirebaseUtils().fireStoreDatabase.collection("stats")
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                querySnapshot.forEach { document ->
+                    Log.d(TAG, "Read document with ID ${document.id}")
+                    Log.d(TAG, "${document.get("max")}");
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents $exception")
+            }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_max)
